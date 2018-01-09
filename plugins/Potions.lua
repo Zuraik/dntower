e8a8c11ab88099cc777d797ba6a6a1cc49b2f39f
camionero <camionero@axy.com>
2018-01-08 23:48:00 -0300

    updated potions plugin to notify upon potions finished

--- plugins/Potions.lua
+++ plugins/Potions.lua
@@ -1,16 +1,26 @@
--- This plugin will use combat haste potions and potions of power attack when needed. Start by typing /chp
 function OnCreate()
     chp = false
+    hasCHP = true
+    hasPPA = true
     this:RegisterCommand("chp", CommandChatType.CHAT_ALLY, CommandAccessLevel.ACCESS_ME)
 end
 
 function OnLTick1s()
     if (chp == true) then
+        inventory = GetInventory()
         me = GetMe()
-        if (me:GotBuff(25094) == false) then
+        if inventory:FindById(23060) == nil and hasPPA then
+            hasPPA = false
+            ShowToClient("Out of", "Potion of Power Attack")
+        end
+        if inventory:FindById(23065) == nil and hasCHP then
+            hasCHP = false
+            ShowToClient("Out of", "Combat Haste Potion")
+        end
+        if (me:GotBuff(25094) == false) and hasPPA then
             UseItem(23060) -- Potion of Power Attack
         end
-        if (me:GotBuff(25099) == false) then
+        if (me:GotBuff(25099) == false) and hasCHP then
             UseItem(23065) -- Combat Haste Potion
         end
     end
